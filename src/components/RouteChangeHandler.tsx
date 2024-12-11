07fc8035238b75056cec0dee2a7e54d530152ddc
James Fitzgerald <james@jawfish.dev>
2024-12-10 23:06:41 -0500

    fix lint

--- src/components/RouteChangeHandler.tsx
+++ src/components/RouteChangeHandler.tsx
@@ -1,11 +1,12 @@
+import { useFileStore } from '@/store/store';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useFileStore } from '@/store/store';
 
 export function RouteChangeHandler() {
   const location = useLocation();
   const resetFileState = useFileStore(state => state.resetFileState);
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: desired behavior
   useEffect(() => {
     resetFileState();
   }, [location.pathname, resetFileState]);
