3e62e3a7507dc807ca70803059b239946927bdcc
James Fitzgerald <james@jawfish.dev>
2024-12-20 07:40:27 -0500

    bun fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 // biome-ignore lint/correctness/noNodejsModules: <explanation>
 import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 // biome-ignore lint/style/noDefaultExport: <explanation>
